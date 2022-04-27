225beb8d6aef580e3ae693841d8ea955a6630724
wzc520pyf <1528857653@qq.com>
2022-04-27 20:58:43 +0800

    feat: 新增文件上传, 新增静态资源服务, 新增entities使用示例(在文件上传端点, 可以为端点的body定义一个实体)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,26 @@ import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhotoModule } from './feature/photo/photo.module';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { RolesGuard } from './common/guard/roles.guard';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './core/auth/auth.module';
-import { JwtAuthGuard } from './core/auth/guards/jwt-auth.guard';
 import { ErrorsInterceptor } from './core/interceptors/errors.interceptor';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { FileModule } from './feature/file/file.module';
 
 @Module({
   // 将 TypeOrmModule 导入AppModule,并使用ormconfig.json中的配置(其中entities - 要加载并用于此连接的实体。接受要加载的实体类和目录路径, synchronize - 指示是否在每次应用程序启动时自动创建数据库架构,生成环境下请务必设为false, 设置autoLoadEntities为true即可自动载入实体---每个通过forFeature()注册的实体都会自动添加到配置对象的entities数组中)
-  imports: [TypeOrmModule.forRoot(), UsersModule, PhotoModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      // 配置静态服务目录---访问: http://localhost:3000/fileUpload/2022-04-27/xxx.png
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    FileModule,
+    UsersModule,
+    PhotoModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,21 @@ async function bootstrap() {
   });
   app.setGlobalPrefix('api/v1'); // 全局路由前缀
   app.use(logger); // 全局日志中间件
+  // 如果遇到请求实体太大的问题, 可揭开下面的注释
+  // app.use(json({ limit: '50mb' }));
+  // app.use(urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
 
 /**
- * 安装mysql相关依赖库: npm install --save @nestjs/typeorm typeorm mysql2
+ * 安装mysql相关依赖库: pnpm install --save @nestjs/typeorm typeorm mysql2
+ * 安装文件上传依赖库: pnpm i -D @types/multer
+ * 安装身份认证依赖库: pnpm install --save @nestjs/passport passport passport-local
+ *                    pnpm install --save-dev @types/passport-local
+ * 安装JWT依赖库:     pnpm install @nestjs/jwt passport-jwt
+ *                    pnpm install @types/passport-jwt --save-dev
+ * 安装静态服务依赖库: pnpm install --save @nestjs/serve-static
+ * 安装工具库: pnpm install dayjs --save // 安装日期格式化库
  */
